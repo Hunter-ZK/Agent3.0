e82deb3c63804a148247d8c6d235dd99efde43ae
Hunter-ZK <55648833+Hunter-ZK@users.noreply.github.com>
2026-09-13 23:53:35 -0400

    feat: add fixed report specification package

---
+++ sql_pilot_engine/spec/__init__.py
@@ -0,0 +1,13 @@
+from sql_pilot_engine.spec.models import (
+    FixedReportField,
+    FixedReportPartition,
+    FixedReportSpec,
+    FixedReportWriteTarget,
+)
+
+__all__ = [
+    "FixedReportField",
+    "FixedReportPartition",
+    "FixedReportSpec",
+    "FixedReportWriteTarget",
+]
